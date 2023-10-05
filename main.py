106341b263e15f25aafd9fa6844686b595ff0a33
satoshi-sh <satoss1108@gmail.com>
2023-10-05 12:08:38 -0500

    Added a baseto the drawing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from turtle import exitonclick
-from drawfunctions import dominic_duffin, david_akim
+from drawfunctions import dominic_duffin, david_akim, satoshi_s
 
 dominic_duffin.christmas_tree()
 david_akim.star()
+satoshi_s.base()
 
 exitonclick()
