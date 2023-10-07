8d360824accca396bfa2283e548165ef8e5bd2c4
Satoshi S <73622805+Satoshi-Sh@users.noreply.github.com>
2023-10-07 10:20:16 -0500

    Added a baseto the drawing (#7)
    * Added a baseto the drawing
    
    * Removed turtle.done()
    
    * Added hideturtle before start drawing for the performance

---
+++ drawfunctions/satoshi_s.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+def base():
+    t = turtle.Turtle()
+    t.color("brown")
+
+    t.hideturtle()
+    t.penup()
+    t.goto(-2, -10)
+    t.pendown()
+
+    # Draw the tree base as a rectangle
+    t.begin_fill()
+    t.forward(40)   # Base width
+    t.left(90)
+    t.forward(20)   # Base height
+    t.left(90)
+    t.forward(40)
+    t.left(90)
+    t.forward(20)
+    t.end_fill()
