332cbab05c859494f72c321cff22ef5597cccfe4
David Akim <davidjohnakim@hotmail.com>
2024-10-12 11:40:42 -0400

    David Art Christmas Sock (#10)
    * Add David Christmas Sock
    
    * Add David Christmas Sock fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from turtle import exitonclick
-from drawfunctions import dominic_duffin, david_akim, satoshi_s, teri_eyenike, david_akim_present_2024
+from drawfunctions import dominic_duffin, david_akim, satoshi_s, teri_eyenike, david_akim_present_2024, david_akim_christmas_sock
 
 dominic_duffin.christmas_tree()
 david_akim.star()
 satoshi_s.base()
 teri_eyenike.draw_shape()
 david_akim_present_2024.draw_present()
+david_akim_christmas_sock.draw_christmas_sock()
 
 exitonclick()
