ca9262959d7773b627d854d470a3c712f45f8ae3
Chris Jarvis <clj@ollure.com>
2024-10-12 17:48:58 -0500

    adding a snowman to drawfunctions (#12)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from turtle import exitonclick
-from drawfunctions import dominic_duffin, david_akim, satoshi_s, teri_eyenike, david_akim_present_2024, david_akim_christmas_sock
+from drawfunctions import dominic_duffin, david_akim, satoshi_s, teri_eyenike, david_akim_present_2024, david_akim_christmas_sock, chris_j
 
 dominic_duffin.christmas_tree()
 david_akim.star()
@@ -7,5 +7,6 @@ satoshi_s.base()
 teri_eyenike.draw_shape()
 david_akim_present_2024.draw_present()
 david_akim_christmas_sock.draw_christmas_sock()
+chris_j.draw_snowman()
 
 exitonclick()
